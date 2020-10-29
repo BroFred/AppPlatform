c5cbb76c2dc71e0f8ff817c088e045c0e97de535
Xingyi Chen <xingyic@splunk.com>
2020-10-29 14:08:16 +0800

    add custom error messages

--- src/App.js
+++ src/App.js
@@ -8,32 +8,42 @@ import {useRef, useEffect, useState } from 'react';
 import Ajv from 'ajv';
 import { toPairs, map, dropWhile } from 'ramda';
 import jsonMap from 'json-source-map';
+import ajvErrors from 'ajv-errors';
 
+// import validation schema
 import schema from './schema/definition.json';
 import tokenSchema from './schema/tokenSchema.json';
 import uqlSchema from './schema/dataSourceSchema.json'
+import vizSchema from './schema/vizSchema.json'
+import formSchema from './schema/formSchema.json'
+import layoutSchema from './schema/layoutSchema.json'
 
-
-var ajv = new Ajv({allErrors: true, jsonPointers: true});
-var validate = ajv.addSchema(tokenSchema).addSchema(uqlSchema).compile(schema);
+const ajv = new Ajv({allErrors: true, jsonPointers: true});
+ajvErrors(ajv)
+const validate = ajv
+.addSchema(tokenSchema)
+.addSchema(uqlSchema)
+.addSchema(vizSchema)
+.addSchema(formSchema)
+.addSchema(layoutSchema)
+.compile(schema);
 
 
 function App() {
   const inputEl = useRef(null);
   const [dataInput, setdataInput] = useState(JSON.stringify({}));
-  const [annotations, setAnnotations] = useState([]);
-  function onChange(newValue) {
+  const [annotationString, setannotationString] = useState("[]");
+  const onChange = (newValue) => {
     setdataInput(newValue)
   }
   useEffect(() => {
     try {
       const { data, pointers } = jsonMap.parse(dataInput);
       var valid = validate(data);
-      const errors = valid? [] : validate.errors
-      console.log(errors)
+      const errors = valid? [] : validate.errors;
       const additionalAnnotations = map(([key, val])=>{ 
         const {additionalProperty} = val.params;
-        return { path: pointers[`${val.dataPath}${additionalProperty ? `/${additionalProperty}`: ''}`].key.line, message: val.message } 
+        return { path: pointers[`${val.dataPath}${additionalProperty ? `/${additionalProperty}`: ''}`].value.line, message: val.message } 
       },toPairs(errors));
       const cunstomAnnotations = map((v)=>({
         row: v.path,
@@ -42,34 +52,34 @@ function App() {
         customized: true,
         type:'error'
       }), additionalAnnotations);
-      const workerAnnotation = dropWhile(({customized})=>customized, annotations);
+      const workerAnnotation = dropWhile(({customized})=>customized, JSON.parse(annotationString));
       inputEl.current.editor.getSession().setAnnotations([...cunstomAnnotations, ...workerAnnotation])
     } catch (error) {
       
     }
-  }, [ dataInput, JSON.stringify(annotations) ])
+  }, [ dataInput, annotationString ])
   
   return (
     <AceEditor
-    ref={inputEl}
-    mode="json"
-    theme="terminal"
-    onChange={onChange}
-    onValidate={setAnnotations}
-    name="UNIQUE_ID_OF_DIV"
-    editorProps={{ $blockScrolling: true }}
-    showPrintMargin={true}
-    showGutter={true}
-    highlightActiveLine={true}
-    value={dataInput}
-    setOptions={{
-      enableBasicAutocompletion: true,
-      enableLiveAutocompletion: true,
-      enableSnippets: true,
-      showLineNumbers: true,
-      tabSize: 2
-    }}
-  />
+      ref={inputEl}
+      mode="json"
+      theme="terminal"
+      onChange={onChange}
+      onValidate={(anno)=>{setannotationString(JSON.stringify(anno))}}
+      name="UNIQUE_ID_OF_DIV"
+      editorProps={{ $blockScrolling: true }}
+      showPrintMargin={true}
+      showGutter={true}
+      highlightActiveLine={true}
+      value={dataInput}
+      setOptions={{
+        enableBasicAutocompletion: true,
+        enableLiveAutocompletion: true,
+        enableSnippets: true,
+        showLineNumbers: true,
+        tabSize: 2
+      }}
+    />
   );
 }
 
